484f1077b9dadc05bbaaa378643b3a7bff65a602
Onatso <nikolai.purtov44@gmail.com>
2024-06-04 21:18:23 +0800

    Проект готов в том виде, в котором я хочу его видеть, остается подгонка под требования + мб добвлю всякую мелочь

--- js/game.js
+++ js/game.js
@@ -1,20 +1,20 @@
-let countryCode=localStorage.getItem("countryCode");
+let countryCode = localStorage.getItem("countryCode");
 let cities;
-if(countryCode=="all") cities = JSON.parse(localStorage.getItem("citiesMapAll"));
-if(countryCode=="RU") cities = JSON.parse(localStorage.getItem("citiesMapRU"));
-if(countryCode=="US") cities = JSON.parse(localStorage.getItem("citiesMapUS"));
+if (countryCode == "all") cities = JSON.parse(localStorage.getItem("citiesMapAll"));
+if (countryCode == "RU") cities = JSON.parse(localStorage.getItem("citiesMapRU"));
+if (countryCode == "US") cities = JSON.parse(localStorage.getItem("citiesMapUS"));
 let difficulty = localStorage.getItem("difficulty");
 const cityNameElement = document.getElementById('city-name');
 let input = document.getElementById("city-input");
 let submitButton = document.getElementById("submit-button");
 let scoreTable = document.getElementById("score");
-let timeSpan=document.getElementById("timer");
+let timeSpan = document.getElementById("timer");
 let score = 0;
 let bool;
 let timeLeft;
 let timer;
 let p;
-scoreTable.textContent = score;
+scoreTable.textContent = "Счёт: " + score;
 console.log(cities);
 
 let size = cities.length;
@@ -24,42 +24,48 @@ let randcity = cities[randIndex1][1][randIndex2];
 cityNameElement.textContent = randcity;
 cities[randIndex1][1].splice(randIndex2, 1);
 
+let q;
+
 for (let k = 0; k < cities.length; k++) {
-  if (cities[k][0] === 'й') p=k;
+  if (cities[k][0] === 'Й') p = k;
+  if (cities[k][0] === 'Ы') q = k;
 }
 
 
-if(randcity.charAt(randcity.length - 1)=='ь') randcity=randcity.substr(0, randcity.length-1);
+if (randcity.charAt(randcity.length - 1) == 'ь') randcity = randcity.substr(0, randcity.length - 1);
 console.log(cities);
 
 startTimer();
 
-function startTimer()
-{
-  if(difficulty=="no-limit") return;
-  if(difficulty=="easy") timeLeft=60*5;
-  if(difficulty=="normal") timeLeft=60;
-  if(difficulty=="hard") timeLeft=15;
-  timer = setInterval(()=>{
+function startTimer() {
+  if (difficulty == "no-limit") return;
+  if (difficulty == "easy") timeLeft = 60 * 5;
+  if (difficulty == "normal") timeLeft = 60;
+  if (difficulty == "hard") timeLeft = 15;
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
+  timeSpan.textContent = `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+  timer = setInterval(() => {
     timeLeft--;
-    timeSpan.textContent=timeLeft;
-    if(timeLeft==0)
-      {
-        clearInterval(timer);
-        cityNameElement.textContent="Время вышло!";
-        cityNameElement.style.color="red";
-        submitButton.style.backgroundColor="#3e8e41";
-        submitButton.disabled=true;
-        return;
-      }
+    minutes = Math.floor(timeLeft / 60);
+    seconds = timeLeft % 60;
+    timeSpan.textContent = `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+    if (timeLeft == 0) {
+      clearInterval(timer);
+      cityNameElement.textContent = "Время вышло!";
+      cityNameElement.style.color = "red";
+      submitButton.style.backgroundColor = "#3e8e41";
+      submitButton.disabled = true;
+      return;
+    }
   }, 1000);
-
 }
-submitButton.addEventListener('click', ()=>{ //доделать букву й
+submitButton.addEventListener('click', () => {
   let inputedCity = input.value;
+  inputedCity = inputedCity.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]"/g, "");
   inputedCity = inputedCity.charAt(0).toUpperCase() + inputedCity.substr(1).toLowerCase();
   for (let i = 0; i < inputedCity.length; i++) {
-    if (inputedCity.charAt(i) === ' '||inputedCity.charAt(i) === '-') inputedCity = inputedCity.substr(0, i + 1) + inputedCity.charAt(i + 1).toUpperCase() + inputedCity.substr(i + 2).toLowerCase();
+    if (inputedCity.charAt(i) === ' ' || inputedCity.charAt(i) === '-') inputedCity = inputedCity.substr(0, i + 1) + inputedCity.charAt(i + 1).toUpperCase() + inputedCity.substr(i + 2).toLowerCase();
   }
   if (randcity.charAt(randcity.length - 1).toUpperCase() === inputedCity.charAt(0)) {
     bool = false;
@@ -72,17 +78,20 @@ submitButton.addEventListener('click', ()=>{ //доделать букву й
             score++;
             clearInterval(timer);
             startTimer();
-            scoreTable.textContent = score;
+            scoreTable.textContent = "Счёт: " + score;
             cities[i][1].splice(j, 1);
-            if(inputedCity.charAt(inputedCity.length - 1)=='ь') inputedCity=inputedCity.substr(0, inputedCity.length-1);
-          //  else if(inputedCity.charAt(inputedCity.length - 1)=='й'&&Array.from(cities[p][1]).length===0) inputedCity=inputedCity.substr(0, inputedCity.length-1);
+            if (inputedCity.charAt(inputedCity.length - 1) == 'ь') inputedCity = inputedCity.substr(0, inputedCity.length - 1);
+            else if (inputedCity.charAt(inputedCity.length - 1) == 'й' && Array.from(cities[p][1]).length === 0) inputedCity = inputedCity.substr(0, inputedCity.length - 1);
+            else if (inputedCity.charAt(inputedCity.length - 1) == 'ы' && Array.from(cities[q][1]).length === 0) inputedCity = inputedCity.substr(0, inputedCity.length - 1);
             for (let k = 0; k < cities.length; k++) {
               if (cities[k][0] === inputedCity.charAt(inputedCity.length - 1).toUpperCase()) {
                 randIndex1 = k;
                 randIndex2 = Math.floor(Array.from(cities[randIndex1][1]).length * Math.random());
                 randcity = cities[randIndex1][1][randIndex2];
                 cityNameElement.textContent = randcity;
-                if(randcity.charAt(randcity.length - 1)=='ь') randcity=randcity.substr(0, randcity.length-1);
+                if (randcity.charAt(randcity.length - 1) == 'ь') randcity = randcity.substr(0, randcity.length - 1);
+                else if (randcity.charAt(randcity.length - 1) == 'й' && Array.from(cities[p][1]).length === 0) randcity = randcity.substr(0, randcity.length - 1);
+                else if (randcity.charAt(randcity.length - 1) == 'ы' && Array.from(cities[p][1]).length === 0) randcity = randcity.substr(0, randcity.length - 1);
                 cities[randIndex1][1].splice(randIndex2, 1);
                 bool = true;
                 break;
@@ -94,39 +103,25 @@ submitButton.addEventListener('click', ()=>{ //доделать букву й
     }
     if (!bool) {
       score--;
-      scoreTable.textContent = score;
+      scoreTable.textContent = "Счёт: " + score;
     }
   }
-  else
-  {
+  else {
     score--;
-    scoreTable.textContent = score;
+    scoreTable.textContent ="Счёт: " + score;
   }
   input.value = '';
 });
 
-cityNameElement.addEventListener('click', () => { //перенести в css
+cityNameElement.addEventListener('click', () => {
   const cityName = cityNameElement.textContent;
-  if(cityName=="Время вышло!") return;
+  if (cityName == "Время вышло!") return;
   const iframeElement = document.createElement('iframe');
   const closeButton = document.createElement('button');
+  closeButton.className = "closebtn";
   iframeElement.src = `https://ru.m.wikipedia.org/wiki/${cityName}`;
-  iframeElement.width = '25%';
-  iframeElement.height = '100%';
-  iframeElement.style.position = 'absolute';
-  iframeElement.style.top = '40px';
-  iframeElement.style.right = '0';
   iframeElement.allowFullscreen = true;
   closeButton.textContent = 'X';
-  closeButton.style.position = 'absolute';
-  closeButton.style.top = '0';
-  closeButton.style.right = '0';
-  closeButton.style.backgroundColor = 'white';
-  closeButton.style.color = 'red';
-  closeButton.style.border = 'none';
-  closeButton.style.padding = '10px 20px';
-  closeButton.style.fontWeight = 'bold';
-  closeButton.style.cursor = 'pointer'; 
   closeButton.addEventListener('click', () => {
     iframeElement.remove();
     closeButton.remove();
@@ -140,14 +135,18 @@ cityNameElement.addEventListener('mouseover', () => {
 });
 
 input.addEventListener('keypress', function (e) {
-  if (e.keyCode === 13) { 
-      submitButton.click(); //ограничить нажатия без введенного текста
+  if (e.keyCode === 13 && input.value !== '') {
+    submitButton.click();
   }
 });
 
-document.getElementById("end-button").addEventListener('click', ()=>{
-  if(localStorage.getItem("record")===null) localStorage.setItem("record", score);
-  else if(parseInt(localStorage.getItem("record"))<score) localStorage.setItem("record", score);
-  window.open('index.html', '_self'); //доделать
+document.getElementById("end-button").addEventListener('click', () => {
+  if (difficulty=="easy"&&localStorage.getItem("recordE") === null) localStorage.setItem("recordE", score);
+  else if (difficulty=="normal"&&localStorage.getItem("recordN") === null) localStorage.setItem("recordN", score);
+  else if (difficulty=="hard"&&localStorage.getItem("recordH") === null) localStorage.setItem("recordH", score);
+  else if (difficulty=="easy"&&parseInt(localStorage.getItem("recordE")) < score) localStorage.setItem("recordE", score);
+  else if (difficulty=="normal"&&parseInt(localStorage.getItem("recordN")) < score) localStorage.setItem("recordN", score);
+  else if (difficulty=="hard"&&parseInt(localStorage.getItem("recordH")) < score) localStorage.setItem("recordH", score);
+  window.open('index.html', '_self'); 
 });
 
--- js/init.js
+++ js/init.js
@@ -1,3 +1,25 @@
+if(localStorage.getItem('recordE')===null) document.getElementById('recordE').textContent="Нет информации";
+else document.getElementById('recordE').textContent=localStorage.getItem('recordE');
+if(localStorage.getItem('recordN')===null) document.getElementById('recordN').textContent="Нет информации";
+else document.getElementById('recordN').textContent=localStorage.getItem('recordN');
+if(localStorage.getItem('recordH')===null) document.getElementById('recordH').textContent="Нет информации";
+else document.getElementById('recordH').textContent=localStorage.getItem('recordH');
+
+let infoButton = document.getElementById('info-button');
+
+infoButton.addEventListener('click', function() {
+  Swal.fire({
+    title: 'Добро пожаловать в Cities-Cross!',
+    text: 'Игра представляет из себя классическую вариацию всеми любимых "городов". Важное замечание: если в базе заканчиваются города на определенную букву, вы можете называть город на предыдущую, компьютер будет делать также.',
+    icon: 'info',
+    confirmButtonText: 'Закрыть',
+    customClass: {
+      container: 'my-swal',
+      popup: 'my-swal-popup'
+    }
+  })
+});
+
 document.getElementById("start-game").addEventListener('click', ()=>{
     let countryCode=document.getElementById("country").value;
     let difficulty=document.getElementById("mode").value;
@@ -6,4 +28,6 @@ document.getElementById("start-game").addEventListener('click', ()=>{
     localStorage.setItem("difficulty", difficulty);
     window.open('loading.html', '_self');
 });
+
+
 
